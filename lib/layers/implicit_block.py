f13edde0ecf92cca1522cf78a4ef61259b38d310
musikisomorphie <wujiqing9@gmail.com>
2021-08-02 15:49:26 +0200

    aa

--- lib/layers/implicit_block.py
+++ lib/layers/implicit_block.py
@@ -140,13 +140,7 @@ class imBlock(nn.Module):
 
         self.nnet_x = nnet_x
         self.nnet_z = nnet_z
-        self.nnet_x_copy = copy.deepcopy(self.nnet_x)
-        self.nnet_z_copy = copy.deepcopy(self.nnet_z)
-        for params in self.nnet_x_copy.parameters():
-            params.requires_grad_(False)
-        for params in self.nnet_z_copy.parameters():
-            params.requires_grad_(False)
-
+        
         self.n_dist = n_dist
         self.geom_p = nn.Parameter(torch.tensor(
             np.log(geom_p) - np.log(1. - geom_p))).float()
@@ -226,6 +220,14 @@ class imBlock(nn.Module):
             return (None, None, dl_dh, dl_dx, *grad_args)
 
     def forward(self, x, logpx=None, restore=False):
+        self.nnet_x_copy = copy.deepcopy(self.nnet_x)
+        self.nnet_z_copy = copy.deepcopy(self.nnet_z)
+        for params in self.nnet_x_copy.parameters():
+            params.requires_grad_(False)
+        for params in self.nnet_z_copy.parameters():
+            params.requires_grad_(False)
+
+
         print(list(self.nnet_x.state_dict().values())[0].get_device())
         print(self.nnet_x.state_dict().keys())
         print(self.nnet_z.state_dict().keys())
