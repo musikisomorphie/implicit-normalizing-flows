88630fac4432d84db4aa153e4d1ea03322cbe06b
musikisomorphie <wujiqing9@gmail.com>
2021-08-02 12:50:46 +0200

    modify run_scrc_im.sh

--- run_scrc_im.sh
+++ run_scrc_im.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python train_img_im.py --data scrc --actnorm True --task hybrid \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_img_im.py --data scrc --actnorm True --task hybrid \
     --nblocks '1-1-1' --idim '512' --act 'swish' --kernels '3-1-3' --vnorms '2222' --fc-end False --preact True \
     --save 'experiments/scrc(blocks_2*3(512,k313)_swish_nofc_preact_10term' --coeff 0.9 --n-exact-terms 10 --imagesize 64 \
     --dataroot /raid/jiqing/Data/SCRC --batchsize 64 --nepochs 100
