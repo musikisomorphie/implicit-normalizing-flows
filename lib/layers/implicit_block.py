af8dcf7ef3b0c3691be2ced60984610d139d878a
musikisomorphie <wujiqing9@gmail.com>
2021-08-02 13:13:39 +0200

    revisie run_scrc_im.sh

--- lib/layers/implicit_block.py
+++ lib/layers/implicit_block.py
@@ -27,6 +27,7 @@ def find_fixed_point(g, y, threshold=1000, eps=1e-5):
             break
     return x
 
+
 def primes(n):
     primfac = []
     d = 2
@@ -39,13 +40,15 @@ def primes(n):
         primfac.append(n)
     return list(set(primfac))
 
+
 def choose_prime(n):
     if n == 32 * 32 * 3:
         return 3079
     elif n == 2:
         return 2
     else:
-        assert False, 'Please specify the prime or the power of a prime given {}'.format(n)
+        assert False, 'Please specify the prime or the power of a prime given {}'.format(
+            n)
 
 
 class RootFind(Function):
@@ -56,9 +59,10 @@ class RootFind(Function):
     @staticmethod
     def banach_find_root(nnet_z, nnet_x, z0, x, *args):
         eps = args[-2]
-        threshold = args[-1]    # Can also set this to be different, based on training/inference
+        # Can also set this to be different, based on training/inference
+        threshold = args[-1]
         x_embed = nnet_x(x) + x
-        g = lambda z: x_embed - nnet_z(z)
+        def g(z): return x_embed - nnet_z(z)
         z_est = find_fixed_point(g, z0, threshold=threshold, eps=eps)
         if threshold > 100:
             torch.cuda.empty_cache()
@@ -67,10 +71,12 @@ class RootFind(Function):
     @staticmethod
     def broyden_find_root(nnet_z, nnet_x, z0, x, *args):
         eps = args[-2]
-        threshold = args[-1]    # Can also set this to be different, based on training/inference
+        # Can also set this to be different, based on training/inference
+        threshold = args[-1]
         x_embed = nnet_x(x) + x
-        g = lambda z: x_embed - nnet_z(z) - z
-        result_info = broyden(g, torch.zeros_like(z0).to(z0), threshold=threshold, eps=eps, name="forward")
+        def g(z): return x_embed - nnet_z(z) - z
+        result_info = broyden(g, torch.zeros_like(z0).to(
+            z0), threshold=threshold, eps=eps, name="forward")
         if result_info['prot_break']:
             z_est = RootFind.banach_find_root(nnet_z, nnet_x, z0, x, eps, 1000)
         else:
@@ -130,7 +136,7 @@ class imBlock(nn.Module):
             brute_force: Computes the exact logdet. Only available for 2D inputs.
         """
         super(imBlock, self).__init__()
-        
+
         self.nnet_x = nnet_x
         self.nnet_z = nnet_z
         self.nnet_x_copy = copy.deepcopy(self.nnet_x)
@@ -141,7 +147,8 @@ class imBlock(nn.Module):
             params.requires_grad_(False)
 
         self.n_dist = n_dist
-        self.geom_p = nn.Parameter(torch.tensor(np.log(geom_p) - np.log(1. - geom_p))).float()
+        self.geom_p = nn.Parameter(torch.tensor(
+            np.log(geom_p) - np.log(1. - geom_p))).float()
         self.lamb = nn.Parameter(torch.tensor(lamb)).float()
         self.n_samples = n_samples
         self.n_power_series = n_power_series
@@ -161,15 +168,14 @@ class imBlock(nn.Module):
         self.register_buffer('last_firmom', torch.zeros(1))
         self.register_buffer('last_secmom', torch.zeros(1))
 
-
     class Backward(Function):
         """
         A 'dummy' function that does nothing in the forward pass and perform implicit differentiation
         in the backward pass. Essentially a wrapper that provides backprop for the `imBlock` class.
         You should use this inner class in imBlock's forward() function by calling:
-        
+
             self.Backward.apply(self.func, ...)
-            
+
         """
         @staticmethod
         def forward(ctx, nnet_z, nnet_x, z, x, *args):
@@ -197,13 +203,15 @@ class imBlock(nn.Module):
                 Fz = nnet_z(z) + z
 
             def g(x_):
-                Fz.backward(x_, retain_graph=True)   # Retain for future calls to g
+                # Retain for future calls to g
+                Fz.backward(x_, retain_graph=True)
                 xJ = z.grad.clone().detach()
                 z.grad.zero_()
                 return xJ - grad
 
             dl_dh = torch.zeros_like(grad).to(grad)
-            result_info = broyden(g, dl_dh, threshold=threshold, eps=eps, name="backward")
+            result_info = broyden(
+                g, dl_dh, threshold=threshold, eps=eps, name="backward")
             dl_dh = result_info['result']
             Fz.backward(torch.zeros_like(dl_dh), retain_graph=False)
 
@@ -216,18 +224,20 @@ class imBlock(nn.Module):
             grad_args = [None for _ in range(len(args))]
             return (None, None, dl_dh, dl_dx, *grad_args)
 
-
     def forward(self, x, logpx=None, restore=False):
         z0 = x.clone().detach()
         if restore:
             with torch.no_grad():
                 _ = self.nnet_x_copy(z0)
                 _ = self.nnet_z_copy(z0)
-        z = RootFind.apply(self.nnet_z, self.nnet_x, z0, z0, 'broyden', self.eps_forward, self.threshold)
-        z = RootFind.f(self.nnet_z, self.nnet_x, z.detach(), z0) + z0 # For backwarding to parameters in func
-        self.nnet_x_copy.load_state_dict(self.nnet_x.state_dict())
-        self.nnet_z_copy.load_state_dict(self.nnet_z.state_dict())
-        z = self.Backward.apply(self.nnet_z_copy, self.nnet_x_copy, z, x, 'broyden', self.eps_backward, self.threshold)
+        z = RootFind.apply(self.nnet_z, self.nnet_x, z0, z0,
+                           'broyden', self.eps_forward, self.threshold)
+        z = RootFind.f(self.nnet_z, self.nnet_x, z.detach(), z0) + \
+            z0  # For backwarding to parameters in func
+        self.nnet_x_copy.load_state_dict(self.nnet_x.module.state_dict())
+        self.nnet_z_copy.load_state_dict(self.nnet_z.module.state_dict())
+        z = self.Backward.apply(self.nnet_z_copy, self.nnet_x_copy,
+                                z, x, 'broyden', self.eps_backward, self.threshold)
         if logpx is None:
             return z
         else:
@@ -235,7 +245,8 @@ class imBlock(nn.Module):
 
     def inverse(self, z, logpy=None):
         x0 = z.clone().detach()
-        x = RootFind.apply(self.nnet_x, self.nnet_z, x0, z, 'broyden', self.eps_sample, self.threshold)
+        x = RootFind.apply(self.nnet_x, self.nnet_z, x0, z,
+                           'broyden', self.eps_sample, self.threshold)
         # x = RootFind.apply(self.nnet_x, self.nnet_z, x0, z, 'banach', self.eps_sample, self.threshold)
         if logpy is None:
             return x
@@ -260,12 +271,13 @@ class imBlock(nn.Module):
                 return (logdet_x - logdet_z).view(-1, 1)
             if self.n_dist == 'geometric':
                 geom_p = torch.sigmoid(self.geom_p).item()
-                sample_fn = lambda m: geometric_sample(geom_p, m)
-                rcdf_fn = lambda k, offset: geometric_1mcdf(geom_p, k, offset)
+                def sample_fn(m): return geometric_sample(geom_p, m)
+                def rcdf_fn(k, offset): return geometric_1mcdf(
+                    geom_p, k, offset)
             elif self.n_dist == 'poisson':
                 lamb = self.lamb.item()
-                sample_fn = lambda m: poisson_sample(lamb, m)
-                rcdf_fn = lambda k, offset: poisson_1mcdf(lamb, k, offset)
+                def sample_fn(m): return poisson_sample(lamb, m)
+                def rcdf_fn(k, offset): return poisson_1mcdf(lamb, k, offset)
 
             if self.training:
                 if self.n_power_series is None:
@@ -273,19 +285,19 @@ class imBlock(nn.Module):
                     lamb = self.lamb.item()
                     n_samples = sample_fn(self.n_samples)
                     n_power_series = max(n_samples) + self.n_exact_terms
-                    coeff_fn = lambda k: 1 / rcdf_fn(k, self.n_exact_terms) * \
+                    def coeff_fn(k): return 1 / rcdf_fn(k, self.n_exact_terms) * \
                         sum(n_samples >= k - self.n_exact_terms) / len(n_samples)
                 else:
                     # Truncated estimation.
                     n_power_series = self.n_power_series
-                    coeff_fn = lambda k: 1.
+                    def coeff_fn(k): return 1.
             else:
                 # Unbiased estimation with more exact terms.
 
                 lamb = self.lamb.item()
                 n_samples = sample_fn(self.n_samples)
                 n_power_series = max(n_samples) + self.n_exact_terms_test
-                coeff_fn = lambda k: 1 / rcdf_fn(k, self.n_exact_terms_test) * \
+                def coeff_fn(k): return 1 / rcdf_fn(k, self.n_exact_terms_test) * \
                     sum(n_samples >= k - self.n_exact_terms_test) / len(n_samples)
 
             if not self.exact_trace:
@@ -294,8 +306,10 @@ class imBlock(nn.Module):
                 ####################################
                 # vareps_x = torch.randn_like(x)
                 # vareps_z = torch.randn_like(z)
-                vareps_x = torch.distributions.bernoulli.Bernoulli(torch.Tensor([0.5])).sample(x.shape).reshape(x.shape).to(x) * 2 - 1
-                vareps_z = torch.distributions.bernoulli.Bernoulli(torch.Tensor([0.5])).sample(z.shape).reshape(z.shape).to(z) * 2 - 1
+                vareps_x = torch.distributions.bernoulli.Bernoulli(
+                    torch.Tensor([0.5])).sample(x.shape).reshape(x.shape).to(x) * 2 - 1
+                vareps_z = torch.distributions.bernoulli.Bernoulli(
+                    torch.Tensor([0.5])).sample(z.shape).reshape(z.shape).to(z) * 2 - 1
 
                 # Choose the type of estimator.
                 if self.training and self.neumann_grad:
@@ -317,8 +331,10 @@ class imBlock(nn.Module):
                     z = z.requires_grad_(True)
                     Fx = self.nnet_x(x)
                     Fz = self.nnet_z(z)
-                    logdet_x = estimator_fn(Fx, x, n_power_series, vareps_x, coeff_fn, self.training)
-                    logdet_z = estimator_fn(Fz, z, n_power_series, vareps_z, coeff_fn, self.training)
+                    logdet_x = estimator_fn(
+                        Fx, x, n_power_series, vareps_x, coeff_fn, self.training)
+                    logdet_z = estimator_fn(
+                        Fz, z, n_power_series, vareps_z, coeff_fn, self.training)
                     logdetgrad = logdet_x - logdet_z
             else:
                 ############################################
@@ -332,22 +348,27 @@ class imBlock(nn.Module):
                 Jx_k = Jx
                 for k in range(2, n_power_series + 1):
                     Jx_k = torch.bmm(Jx, Jx_k)
-                    logdetJx = logdetJx + (-1)**(k+1) / k * coeff_fn(k) * batch_trace(Jx_k)
+                    logdetJx = logdetJx + (-1)**(k+1) / \
+                        k * coeff_fn(k) * batch_trace(Jx_k)
                 Fz = self.nnet_z(z)
                 Jz = batch_jacobian(Fz, z)
                 logdetJz = batch_trace(Jz)
                 Jz_k = Jz
                 for k in range(2, n_power_series + 1):
                     Jz_k = torch.bmm(Jz, Jz_k)
-                    logdetJz = logdetJz + (-1)**(k+1) / k * coeff_fn(k) * batch_trace(Jz_k)
+                    logdetJz = logdetJz + (-1)**(k+1) / \
+                        k * coeff_fn(k) * batch_trace(Jz_k)
                 logdetgrad = logdetJx - logdetJz
 
             if self.training and self.n_power_series is None:
-                self.last_n_samples.copy_(torch.tensor(n_samples).to(self.last_n_samples))
+                self.last_n_samples.copy_(torch.tensor(
+                    n_samples).to(self.last_n_samples))
                 estimator = logdetgrad.detach()
-                self.last_firmom.copy_(torch.mean(estimator).to(self.last_firmom))
-                self.last_secmom.copy_(torch.mean(estimator**2).to(self.last_secmom))
-            return logdetgrad.view(-1, 1) 
+                self.last_firmom.copy_(torch.mean(
+                    estimator).to(self.last_firmom))
+                self.last_secmom.copy_(torch.mean(
+                    estimator**2).to(self.last_secmom))
+            return logdetgrad.view(-1, 1)
 
     def extra_repr(self):
         return 'dist={}, n_samples={}, n_power_series={}, neumann_grad={}, exact_trace={}, brute_force={}, neumann_grad={}, grad_in_forward={}'.format(
@@ -358,13 +379,13 @@ class imBlock(nn.Module):
 def batch_jacobian(g, x, create_graph=True):
     jac = []
     for d in range(g.shape[1]):
-        jac.append(torch.autograd.grad(torch.sum(g[:, d]), x, create_graph=create_graph)[0].view(x.shape[0], 1, x.shape[1]))
+        jac.append(torch.autograd.grad(torch.sum(g[:, d]), x, create_graph=create_graph)[
+                   0].view(x.shape[0], 1, x.shape[1]))
     return torch.cat(jac, 1)
 
 
 def batch_trace(M):
     return M.view(M.shape[0], -1)[:, ::M.shape[1] + 1].sum(1)
-
 
 
 #####################
@@ -380,7 +401,8 @@ class MemoryEfficientLogDetEstimator(torch.autograd.Function):
             g = gnet(x)
             ctx.g = g
             ctx.x = x
-            logdetgrad = estimator_fn(g, x, n_power_series, vareps, coeff_fn, training)
+            logdetgrad = estimator_fn(
+                g, x, n_power_series, vareps, coeff_fn, training)
 
             if training:
                 grad_x, *grad_params = torch.autograd.grad(
@@ -410,7 +432,8 @@ class MemoryEfficientLogDetEstimator(torch.autograd.Function):
         dL = grad_logdetgrad[0].detach()
         with torch.no_grad():
             grad_x.mul_(dL)
-            grad_params = tuple([g.mul_(dL) if g is not None else None for g in grad_params])
+            grad_params = tuple(
+                [g.mul_(dL) if g is not None else None for g in grad_params])
 
         return (None, None, grad_x, None, None, None, None) + grad_params
 
@@ -419,8 +442,10 @@ def basic_logdet_estimator(g, x, n_power_series, vareps, coeff_fn, training):
     vjp = vareps
     logdetgrad = torch.tensor(0.).to(x)
     for k in range(1, n_power_series + 1):
-        vjp = torch.autograd.grad(g, x, vjp, create_graph=training, retain_graph=True)[0]
-        tr = torch.sum(vjp.view(x.shape[0], -1) * vareps.view(x.shape[0], -1), 1)
+        vjp = torch.autograd.grad(
+            g, x, vjp, create_graph=training, retain_graph=True)[0]
+        tr = torch.sum(vjp.view(x.shape[0], -1)
+                       * vareps.view(x.shape[0], -1), 1)
         delta = (-1)**(k + 1) / k * coeff_fn(k) * tr
         logdetgrad = logdetgrad + delta
     return logdetgrad
@@ -434,7 +459,8 @@ def neumann_logdet_estimator(g, x, n_power_series, vareps, coeff_fn, training):
             vjp = torch.autograd.grad(g, x, vjp, retain_graph=True)[0]
             neumann_vjp = neumann_vjp + (-1)**k * coeff_fn(k) * vjp
     vjp_jac = torch.autograd.grad(g, x, neumann_vjp, create_graph=training)[0]
-    logdetgrad = torch.sum(vjp_jac.view(x.shape[0], -1) * vareps.view(x.shape[0], -1), 1)
+    logdetgrad = torch.sum(vjp_jac.view(
+        x.shape[0], -1) * vareps.view(x.shape[0], -1), 1)
     return logdetgrad
 
 
@@ -446,7 +472,8 @@ def mem_eff_wrapper(estimator_fn, gnet, x, n_power_series, vareps, coeff_fn, tra
         raise ValueError('g is required to be an instance of nn.Module.')
 
     return MemoryEfficientLogDetEstimator.apply(
-        estimator_fn, gnet, x, n_power_series, vareps, coeff_fn, training, *list(gnet.parameters())
+        estimator_fn, gnet, x, n_power_series, vareps, coeff_fn, training, *
+        list(gnet.parameters())
     )
 
 
@@ -500,5 +527,6 @@ def _flatten(sequence):
 
 
 def _flatten_convert_none_to_zeros(sequence, like_sequence):
-    flat = [p.reshape(-1) if p is not None else torch.zeros_like(q).view(-1) for p, q in zip(sequence, like_sequence)]
+    flat = [p.reshape(-1) if p is not None else torch.zeros_like(q).view(-1)
+            for p, q in zip(sequence, like_sequence)]
     return torch.cat(flat) if len(flat) > 0 else torch.tensor([])
