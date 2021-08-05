ca29be59bb876181e242ae63e0160ceffa08c57e
musikisomorphie <wujiqing9@gmail.com>
2021-08-05 14:31:54 +0200

    test

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train_img_re.py --data scrc --actnorm True --task hybrid \
-    --nblocks 4-4-4 \
-    --save 'experiments/scrc_re' --imagesize 64 \
-    --dataroot /raid/jiqing/Data/SCRC --batchsize 32 --nepochs 100
+    --nblocks 2-2-2 \
+    --save 'experiments/scrc_re' --imagesize 256 \
+    --dataroot /raid/jiqing/Data/SCRC --batchsize 16 --nepochs 100
