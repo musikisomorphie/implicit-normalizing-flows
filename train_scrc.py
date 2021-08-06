1133a02416457610961e1e8e957cce6ba291276c
musikisomorphie <wujiqing9@gmail.com>
2021-08-06 11:14:22 +0200

    add an resnet training model wihtout flow

--- train_scrc.py
+++ train_scrc.py
@@ -1,240 +1,239 @@
-import logging
-from Data import SCRC_dict
 import argparse
-import json
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.optim import lr_scheduler
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-from torch.utils.data import DataLoader, SequentialSampler, BatchSampler, RandomSampler
-import matplotlib.pyplot as plt
 import time
+import math
 import os
-import copy
-from pathlib import Path
-from random import shuffle
-# from VISION.utils import custom_logger, initialize_model, get_data_loader, get_transform, SCRC
-import sys
-sys.path.append('.')
+import os.path
+import numpy as np
+from tqdm import tqdm
+import gc
+import pathlib
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import torchvision.datasets as vdsets
+
+from lib.resflow import ACT_FNS, ResidualFlow
+import lib.datasets as datasets
+import lib.optimizers as optim
+import lib.utils as utils
+import lib.layers as layers
+import lib.layers.base as base_layers
+from lib.lr_scheduler import CosineAnnealingWarmRestarts
 
 
-def compute_acc(grads, grad_labs, phase):
-    """Compute the classification accuracies achieved by trained model 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--data', type=str, default='cifar10', choices=[
+        'mnist',
+        'cifar10',
+        'svhn',
+        'celebahq',
+        'celeba_5bit',
+        'imagenet32',
+        'imagenet64',
+        'scrc'
+    ]
+)
+parser.add_argument('--classifier', type=str, default='resnet',
+                    choices=['resnet', 'densenet'])
+parser.add_argument('--imagesize', type=int, default=32)
+parser.add_argument('--dataroot', type=str, default='data')
+parser.add_argument('--nbits', type=int, default=8)  # Only used for celebahq.
 
-    Args:
-        grads: the predicted grades
-        grad_labs: the ground-truth grades
-        phase: train, val, test
+parser.add_argument('--block', type=str,
+                    choices=['resblock', 'coupling'], default='resblock')
 
-    Returns:
-        all the acc info saved in the logger
-        the acc for normal, low grade, and high grade TMA
-    """
-    acc_tot = torch.sum(grads == grad_labs.data)
-    norml_tp = torch.sum((grads == 0) & (grad_labs.data == 0))
-    norml_tot = torch.sum(grad_labs.data == 0)
+parser.add_argument('--coeff', type=float, default=0.98)
+parser.add_argument('--vnorms', type=str, default='2222')
+parser.add_argument('--n-lipschitz-iters', type=int, default=None)
+parser.add_argument('--sn-tol', type=float, default=1e-3)
+parser.add_argument('--learn-p', type=eval,
+                    choices=[True, False], default=False)
 
-    low_tp = torch.sum((grads == grad_labs.data) & (grad_labs.data == 1))
-    low_tot = torch.sum(grad_labs.data == 1)
+parser.add_argument('--n-power-series', type=int, default=None)
+parser.add_argument('--factor-out', type=eval,
+                    choices=[True, False], default=False)
+parser.add_argument(
+    '--n-dist', choices=['geometric', 'poisson'], default='poisson')
+parser.add_argument('--n-samples', type=int, default=1)
+parser.add_argument('--n-exact-terms', type=int, default=2)
+parser.add_argument('--var-reduc-lr', type=float, default=0)
+parser.add_argument('--neumann-grad', type=eval,
+                    choices=[True, False], default=True)
+parser.add_argument('--mem-eff', type=eval,
+                    choices=[True, False], default=True)
 
-    high_tp = torch.sum((grads == grad_labs.data) & (grad_labs.data == 2))
-    high_tot = torch.sum(grad_labs.data == 2)
+parser.add_argument('--act', type=str, choices=ACT_FNS.keys(), default='swish')
+parser.add_argument('--idim', type=int, default=512)
+parser.add_argument('--nblocks', type=str, default='16-16-16')
+parser.add_argument('--squeeze-first', type=eval,
+                    default=False, choices=[True, False])
+parser.add_argument('--actnorm', type=eval,
+                    default=True, choices=[True, False])
+parser.add_argument('--fc-actnorm', type=eval,
+                    default=False, choices=[True, False])
+parser.add_argument('--batchnorm', type=eval,
+                    default=False, choices=[True, False])
+parser.add_argument('--dropout', type=float, default=0.)
+parser.add_argument('--fc', type=eval, default=False, choices=[True, False])
+parser.add_argument('--kernels', type=str, default='3-1-3')
+parser.add_argument('--add-noise', type=eval,
+                    choices=[True, False], default=True)
+parser.add_argument('--quadratic', type=eval,
+                    choices=[True, False], default=False)
+parser.add_argument('--fc-end', type=eval, choices=[True, False], default=True)
+parser.add_argument('--fc-idim', type=int, default=128)
+parser.add_argument('--preact', type=eval, choices=[True, False], default=True)
+parser.add_argument('--padding', type=int, default=0)
+parser.add_argument('--first-resblock', type=eval,
+                    choices=[True, False], default=True)
+parser.add_argument('--cdim', type=int, default=256)
 
-    tumor_tp = torch.sum((grads != 0) & (grad_labs.data != 0))
-    tumor_fp = torch.sum((grads != 0) & (grad_labs.data == 0))
-    tumor_fn = torch.sum((grads == 0) & (grad_labs.data != 0))
+parser.add_argument('--optimizer', type=str,
+                    choices=['adam', 'adamax', 'rmsprop', 'sgd'], default='adam')
+parser.add_argument('--scheduler', type=eval,
+                    choices=[True, False], default=False)
+parser.add_argument(
+    '--nepochs', help='Number of epochs for training', type=int, default=1000)
+parser.add_argument('--batchsize', help='Minibatch size', type=int, default=64)
+parser.add_argument('--lr', help='Learning rate', type=float, default=1e-3)
+parser.add_argument('--wd', help='Weight decay', type=float, default=0)
+parser.add_argument('--warmup-iters', type=int, default=1000)
+parser.add_argument('--annealing-iters', type=int, default=0)
+parser.add_argument('--save', help='directory to save results',
+                    type=str, default='experiment1')
+parser.add_argument('--val-batchsize',
+                    help='minibatch size', type=int, default=200)
+parser.add_argument('--seed', type=int, default=None)
+parser.add_argument('--ema-val', type=eval,
+                    choices=[True, False], default=True)
+parser.add_argument('--update-freq', type=int, default=1)
 
-    acc_tot = acc_tot.float() / grads.size()[0]
+parser.add_argument('--task', type=str,
+                    choices=['density', 'classification', 'hybrid'], default='density')
+parser.add_argument('--scale-dim', type=eval,
+                    choices=[True, False], default=False)
+parser.add_argument('--rcrop-pad-mode', type=str,
+                    choices=['constant', 'reflect'], default='reflect')
+parser.add_argument('--padding-dist', type=str,
+                    choices=['uniform', 'gaussian'], default='uniform')
 
-    acc_norml = norml_tp.float() / norml_tot
-    acc_low = low_tp.float() / low_tot
-    acc_high = high_tp.float() / high_tot
+parser.add_argument('--resume', type=str, default=None)
+parser.add_argument('--begin-epoch', type=int, default=0)
 
-    acc_tumor = (norml_tp.float() + tumor_tp.float()) / grads.size()[0]
+parser.add_argument('--nworkers', type=int, default=8)
+parser.add_argument(
+    '--print-freq', help='Print progress every so iterations', type=int, default=20)
+parser.add_argument(
+    '--vis-freq', help='Visualize progress every so iterations', type=int, default=500)
+args = parser.parse_args()
 
-    acc_info = ('{} Total acc: {:.2f}%, Normal acc: {:.2f}%, Tumor acc: {:.2f}%, \n'
-                '   low acc: {:.2f}%, high acc: {:.2f}% \n'
-                '   Tumor Precision: {:.2f}%, Recall: {:.2f}%'). \
-        format(phase, acc_tot * 100, acc_norml * 100, acc_tumor * 100,
-               acc_low * 100, acc_high * 100,
-               tumor_tp * 100 / (tumor_tp + tumor_fp), tumor_tp * 100 / (tumor_tp + tumor_fn))
-    return acc_info, acc_norml, acc_low, acc_high
+# Random seed
+if args.seed is None:
+    args.seed = np.random.randint(100000)
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+torch.backends.cudnn.benchmark = True
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if device.type == 'cuda':
+    torch.cuda.manual_seed(args.seed)
 
-def train_model(batch,
-                device,
-                model,
-                dataloaders,
-                criterion,
-                optimizer,
-                scheduler,
-                logging,
-                dir_pt,
-                num_epochs=25):
-    """The main function for training scrc,
-    the output is grade classification, 0 Normal TMA, 1 low grade TMA, 2 high grade TMA.
+trn_trans = transforms.Compose([
+    transforms.RandomCrop(args.imagesize),
+    transforms.ColorJitter(),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+])
 
-    Args:
-        exp: the experiment id 
-        batch: batch size for training
-        device: CUDA or CPU
-        model: the model architecture resnet, densenet, etc.
-        dataloader: the dataloader for train, val, test
-        criterion: the loss function -- crossentropy
-        opitmizer: the optimizer -- Adam
-        scheduler: learning rate sceduler, decay LR after certain step size
-        logging:  saved logging information with neat format
-        dir_pt: path to the output pt file with good accuracy
-        num_epochs: the training epochs
-    """
+tst_trans = transforms.Compose([
+    transforms.CenterCrop(args.imagesize)
+])
 
-    since = time.time()
-    best_score = float('-inf')
+dat_path = str(pathlib.Path(args.dataroot) / 'scrc_symm_{}.pt')
+scrc_in = [0, 1, 2]
+scrc_out = 'cms'
+trn_reg = ['0', '2']
+tst_reg = '1'
+tst_size = 384
 
-    for epoch in range(num_epochs):
-        logging.info('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        logging.info('-' * 10)
+im_dim = len(scrc_in)
+n_classes = 4
 
-        # Each epoch has a training and validation phase
-        for phase in ('train', 'val', 'test'):
-            dt_num = len(dataloaders[phase].dataset)
-            grads_total = torch.zeros(dt_num, device=device)
-            grad_labs_total = torch.zeros(dt_num, device=device)
-            loss = 0.
+trn_data, trn_loader = list(), list()
+for trn in trn_reg:
+    trn_data.append(datasets.SCRC(dat_path.format(trn),
+                                  scrc_in=scrc_in,
+                                  scrc_out=scrc_out,
+                                  transforms=trn_trans))
+    trn_loader.append(torch.utils.data.DataLoader(trn_data[-1],
+                                                  batch_size=args.batchsize,
+                                                  shuffle=True,
+                                                  num_workers=args.nworkers,
+                                                  drop_last=True))
 
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+tst_path = dat_path.format(tst_reg)
+tst_len = torch.load(str(tst_path))[0].shape[0]
+print('test data size {}'.format(tst_len))
+tst_idx = np.random.rand(tst_len).argsort()
+tst_data, tst_loader = list(), list()
+for i in range(2):
+    tst_sub_idx = tst_idx[:tst_size] if i == 0 else tst_idx[tst_size:]
+    tst_data.append(datasets.SCRC(tst_path,
+                                  tst_sub_idx,
+                                  scrc_in,
+                                  scrc_out,
+                                  transforms=tst_trans))
 
-            for idx, dt_scrc in enumerate(dataloaders[phase]):
-                imgs = dt_scrc[0].to(device)
-                labs = dt_scrc[1].to(device)
-                grad_dim = SCRC_dict['map']['G'][0]
-                grad_labs = labs[:, grad_dim].long()
-                optimizer.zero_grad()
-                with torch.set_grad_enabled(phase == 'train'):
-                    if phase == 'train':
-                        inputs = imgs
-                        grad = model(inputs)
-                        loss_grad = criterion(grad, grad_labs)
-                        loss_grad.backward()
-                        optimizer.step()
-                        loss += loss_grad.item() * inputs.size(0)
-                    else:
-                        inputs = imgs
-                        grad_many = model(inputs)
-                        grad = grad_many
-
-                    _, class_grad = torch.max(grad, dim=1)
-                    grads_total[idx * batch:(idx + 1) * batch] = class_grad
-                    grad_labs_total[idx * batch:(idx + 1) * batch] = grad_labs
-
-            acc_info, acc_norml, acc_low, acc_high = compute_acc(
-                grads_total, grad_labs_total, phase)
-            cur_score = acc_norml + acc_low + acc_high
-            cur_cond = acc_norml >= 0.9 and acc_low >= 0.8 and acc_high >= 0.55
-
-            if phase == 'train':
-                scheduler.step()
-                logging.info('{} CrosEntropyLoss: {:.4f}'.format(
-                    phase, loss / dt_num))
-            elif phase == 'test':
-                acc_info += '\n \n'
-            elif phase == 'val' and cur_score > best_score and cur_cond:
-                logging.info(
-                    'save the model with best score {} at epoch {}'.format(cur_score, epoch))
-                best_score = cur_score
-                model_file = str(dir_pt) + '_{}.pt'.format(epoch)
-                torch.save(model.state_dict(), model_file)
-
-            logging.info(acc_info)
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_score))
-    return
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='cnn backbones for Swiss CRC')
-    parser.add_argument('--data-dir', type=Path, default='/home/histopath/Data/SCRC/', metavar='DIR',
-                        help='input batch size for training')
-    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
-                        help='input batch size for training (default: 32)')
-    parser.add_argument('--test-batch-size', type=int, default=32, metavar='N',
-                        help='input batch size for testing (default: 8)')
-    parser.add_argument('--epochs', type=int, default=200, metavar='N',
-                        help='number of epochs to train (default: 25)')
-    parser.add_argument('--lr', type=float, default=0.002, metavar='LR',
-                        help='learning rate (default: 0.001)')
-    parser.add_argument('--gamma', type=float, default=0.5, metavar='M',
-                        help='Learning rate step gamma (default: 0.1)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--save-results', action='store_true', default=False,
-                        help='Save the predicted results for causal inference')
-    args = parser.parse_args()
-
-    torch.manual_seed(args.seed)
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    # model configuration
-    exp_dir = args.data_dir / 'experiments'
-    # 'resnet', 'densenet', 'shufflenet', 'mobilenet'
-    for md_name in ('densenet',):
-        for id_exp in range(4):
-            model = initialize_model(md_name,
-                                     num_classes=3,
-                                     num_pred=0).to(device)
-
-            # generate the data loader for train, val and test
-            data_loader = dict()
-            for phase in ('train', 'val', 'test'):
-                bat = args.batch_size if phase == 'train' else args.test_batch_size
-                dt_trans = get_transform('scrc', phase)
-                data_scrc = SCRC(str(args.data_dir /
-                                     'gt_{}.pt'.format(phase)),
-                                 transform=dt_trans)
-                dt_loader = get_data_loader(data_scrc, bat, seq=False)
-                data_loader[phase] = dt_loader
-                print('{} data_loader done.'.format(phase))
-
-            criterion = nn.CrossEntropyLoss()
-
-            optimizer_ft = optim.AdamW(model.parameters(), lr=args.lr)
-
-            # Decay LR by a factor of 0.1 every 20 epochs
-            exp_lr_scheduler = lr_scheduler.StepLR(
-                optimizer_ft, gamma=args.gamma, step_size=20)
-
-            single_exp_dir = exp_dir / ('cnn_{}'.format(id_exp))
-            logger = custom_logger(
-                str(single_exp_dir / ('{}.log'.format(md_name))))
-
-            dir_pt = args.data_dir / 'experiments' / \
-                'cnn_{}'.format(id_exp) / '{}'.format(md_name)
-
-            train_model(args.batch_size,
-                        device,
-                        model,
-                        data_loader,
-                        criterion,
-                        optimizer_ft,
-                        exp_lr_scheduler,
-                        logger,
-                        dir_pt,
-                        num_epochs=args.epochs)
+    tst_loader.append(torch.utils.data.DataLoader(tst_data[-1],
+                                                  batch_size=args.val_batchsize,
+                                                  shuffle=False,
+                                                  num_workers=args.nworkers,
+                                                  drop_last=True))
 
 
-if __name__ == '__main__':
-    main()
+model = utils.initialize_model(args.classifier,
+                               num_classes=n_classes,
+                               chn_dim=im_dim).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+criterion = torch.nn.CrossEntropyLoss()
+
+for epoch in range(args.begin_epoch, args.nepochs):
+    model.train()
+    total, correct = 0, 0
+    trn_iter = iter(trn_loader[0])
+    for i, (x_1, y_1) in enumerate(trn_loader[1]):
+        try:
+            (x_0, y_0) = next(trn_iter)
+        except StopIteration:
+            trn_iter = iter(trn_loader[0])
+            (x_0, y_0) = next(trn_iter)
+
+        optimizer.zero_grad()
+
+        x = torch.cat((x_0, x_1), dim=0)
+        y = torch.cat((y_0, y_1), dim=0)
+        bat_id = np.random.rand(x.shape[0]).argsort()
+        x = x[bat_id, ]
+        y = y[bat_id, ]
+        x = x.to(device)
+        y = y.to(device)
+
+        logits = model(x)
+        loss = criterion(logits, y)
+
+        _, predicted = logits.max(1)
+        total += y.size(0)
+        correct += predicted.eq(y).sum().item()
+
+        loss.backward()
+        optimizer.step()
+
+        if i % args.print_freq == 0:
+            print('Epoch: {} | Iter: {} | Acc: {0:.4f}'.format(
+                epoch, i, 100. * correct / total))
