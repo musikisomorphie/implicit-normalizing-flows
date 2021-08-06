e58e0ce0e192cc52a44451222ceef18f74c937c5
musikisomorphie <wujiqing9@gmail.com>
2021-08-06 11:34:36 +0200

    add an resnet training model wihtout flow

--- run_scrc.sh
+++ run_scrc.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python train_scrc.py --data scrc --actnorm True --task hybrid \
+CUDA_VISIBLE_DEVICES=0,1 python train_scrc.py --data scrc --actnorm True --task hybrid \
     --nblocks 1-1-1 --print-freq 40 \
     --save 'experiments/scrc' --imagesize 256 \
     --dataroot /raid/jiqing/Data/SCRC --batchsize 32 --nepochs 100
