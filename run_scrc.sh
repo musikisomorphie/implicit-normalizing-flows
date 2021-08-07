6e2f106c2d42f58340599c5b0c6d02fc118497f3
musikisomorphie <wujiqing9@gmail.com>
2021-08-07 14:34:40 +0200

    visual exam

--- run_scrc.sh
+++ run_scrc.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3 python train_scrc.py --data scrc --actnorm True --task hybrid \
+CUDA_VISIBLE_DEVICES=4 python train_scrc.py --data scrc --actnorm True --task hybrid \
     --nblocks 1-1-1 --print-freq 60 \
-    --save 'experiments/scrc_im_tp_rc_hed_rr' --imagesize 256 \
+    --save 'experiments/scrc_im_tp_rc_hed' --imagesize 256 \
     --dataroot /raid/jiqing/Data/SCRC --batchsize 32 --val-batchsize 64 --nepochs 100
