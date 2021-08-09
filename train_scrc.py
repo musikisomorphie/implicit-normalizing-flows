73b4964f49a3142be4720a67f75e8b002014bef4
musikisomorphie <wujiqing9@gmail.com>
2021-08-09 12:36:47 +0200

    visual exam

--- train_scrc.py
+++ train_scrc.py
@@ -141,12 +141,12 @@ if device.type == 'cuda':
 
 trn_trans = transforms.Compose([
     transforms.RandomCrop(args.imagesize),
-    utils.HEDJitter(0.05),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
-    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
-    transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+    # utils.HEDJitter(0.05),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomVerticalFlip(),
+    # transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+    # transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
+    # transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.5),
 ])
 
 tst_trans = transforms.Compose([
@@ -156,8 +156,8 @@ tst_trans = transforms.Compose([
 dat_path = str(pathlib.Path(args.dataroot) / 'scrc_symm_{}.pt')
 scrc_in = [4]
 scrc_out = 'cms'
-trn_reg = ['0', '2']
-tst_reg = '1'
+trn_reg = ['1', '2']
+tst_reg = '0'
 tst_size = 384
 
 im_dim = len(scrc_in)
