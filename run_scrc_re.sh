978d7f05980783b017733a377b7d03f905ce21cf
musikisomorphie <wujiqing9@gmail.com>
2021-08-09 17:21:29 +0200

    try distributed parallel

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch train_img_re --data scrc --actnorm True --task hybrid \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch train_img_re.py --data scrc --actnorm True --task hybrid \
     --nblocks 4-4-4 \
     --save 'experiments/scrc_re' --imagesize 128 \
     --dataroot /raid/jiqing/Data/SCRC --batchsize 16 --val-batchsize 32 --nepochs 100
