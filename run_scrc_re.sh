85317f8c1088196a731219232ec3c3a53b1203e7
musikisomorphie <wujiqing9@gmail.com>
2021-08-11 15:25:05 +0200

    zero optim

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -2,4 +2,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed train_img_re.py --cuda  --data scrc --dee
     --actnorm True --task hybrid --nworkers 2 \
     --nblocks 4-4-4 \
     --save 'experiments/scrc_re' --imagesize 128 \
-    --dataroot /raid/jiqing/Data/SCRC --batchsize 16 --val-batchsize 16 --nepochs 100
+    --dataroot /raid/jiqing/Data/SCRC --batchsize 12 --val-batchsize 24 --nepochs 100
