4f590c6db58bc14c83e795ee37683ed62f1e7f9f
musikisomorphie <wujiqing9@gmail.com>
2021-08-13 10:53:16 +0200

    train_img_re.py

--- train_img_re.py
+++ train_img_re.py
@@ -774,7 +774,7 @@ def train(epoch, model, trn_loader):
             if not args.scale_dim:
                 bpd = bpd * (args.imagesize * args.imagesize * im_dim)
             # Change cross entropy from nats to bits.
-            loss = bpd + crossent / np.log(2)
+            loss = crossent / np.log(2)
         loss.backward()
 
         if global_itr % args.update_freq == args.update_freq - 1:
