0f82552477b3ca27b713be8200e33c054b01464d
musikisomorphie <wujiqing9@gmail.com>
2021-08-13 09:56:44 +0200

    tested locally

--- run_scrc_re_local.sh
+++ run_scrc_re_local.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1 deepspeed train_img_re.py --cuda  --data scrc --deepspeed_config config_re.json \
-    --actnorm True --task hybrid \
-    --nblocks 1-1-1 \
+    --actnorm True --task hybrid --nworkers 1 \
+    --nblocks 2-2-2 \
     --save 'experiments/scrc_re' --imagesize 64 \
     --dataroot /home/miashan/Data/SCRC --batchsize 8 --val-batchsize 8 --nepochs 100
