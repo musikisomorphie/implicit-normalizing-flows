9f387e9bef7f0592e682701ffdd7a7f287e80758
musikisomorphie <wujiqing9@gmail.com>
2021-08-17 09:50:02 +0200

    smaller nblocks

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed train_img_re.py --cuda  --data scrc --deepspeed_config config_re.json \
     --actnorm True --task density --nworkers 2 \
-    --nblocks 16-16-16 --print-freq 60 --factor-out True --squeeze-first True \
+    --nblocks 14-14-14 --print-freq 60 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_re_256' --imagesize 256 \
     --dataroot /raid/jiqing/Data/SCRC --batchsize 16 --val-batchsize 32 --nepochs 100
