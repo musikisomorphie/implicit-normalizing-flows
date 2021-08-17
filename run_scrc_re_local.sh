1a121b9ce259f78b5b11ba8f79fef4639cc62dca
musikisomorphie <wujiqing9@gmail.com>
2021-08-17 09:35:38 +0200

    transform up to 256

--- run_scrc_re_local.sh
+++ run_scrc_re_local.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1 deepspeed train_img_re.py --cuda  --data scrc --deepspeed_config config_re.json \
     --actnorm True --task density --nworkers 1 \
-    --nblocks 2-2-2 --factor-out True --squeeze-first True \
-    --save 'experiments/scrc_re' --imagesize 64 \
+    --nblocks 2-2-2 --print-freq 60 --factor-out True --squeeze-first True \
+    --save 'experiments/scrc_re' --imagesize 256 \
     --dataroot /home/miashan/Data/SCRC --batchsize 8 --val-batchsize 8 --nepochs 100
