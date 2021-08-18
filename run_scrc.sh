d74e997e814c3c5abeb0b54e0e9265ecf19121e5
musikisomorphie <wujiqing9@gmail.com>
2021-08-18 11:19:42 +0200

    revise train_scrc

--- run_scrc.sh
+++ run_scrc.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=0 python train_scrc.py --cuda  --data scrc \
     --actnorm True --task classification \
     --nblocks 1-1-1 --print-freq 60 --squeeze-first True \
     --save 'experiments/scrc_256_128_decouple/' --imagesize 256 \
-    --dataroot /home/histopath/Data/SCRC_nuclei/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
+    --dataroot /raid/jiqing/Data/SCRC/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
     --env '201' --aug 'rr' --inp 'i'
