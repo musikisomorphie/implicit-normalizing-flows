1de419d65a20b060b98370edba3add1ee9d45904
musikisomorphie <wujiqing9@gmail.com>
2021-08-20 17:15:40 +0200

    refactor the codes

--- run_scrc_im.sh
+++ run_scrc_im.sh
@@ -1,6 +1,6 @@
-deepspeed --include=localhost:0,1,2,3 --master_port 50123 train_im.py --cuda  --data scrc --deepspeed_config config_im.json \
+deepspeed --include=localhost:0 --master_port 50123 train_im.py --cuda  --data scrc --deepspeed_config config_im.json \
     --actnorm True --task hybrid --nworkers 2 \
-    --nblocks 10-10-10 --print-freq 120 --factor-out True --squeeze-first True \  
+    --nblocks 10-10-10 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_im_alpha/' --imagesize 256 \
     --dataroot /raid/jiqing/Data/SCRC/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
     --env '201' --aug 'rr' --inp 'i' \
