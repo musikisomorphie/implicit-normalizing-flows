cfd7606ac147f2a7770aa91e6203d5d4c01bfc0d
musikisomorphie <wujiqing9@gmail.com>
2021-08-20 18:21:20 +0200

    refactor codes

--- lib/implicit_flow.py
+++ lib/implicit_flow.py
@@ -112,8 +112,7 @@ class ImplicitFlow(nn.Module):
             class_chn = input_size[1] * (scale_factor**2) // 4
             self.build_classifier(class_chn)
 
-        self.fixed_z = utils.standard_normal_sample([input_size[0] * 2,
-                                                     *self.trans_size[1:]])
+        self.fixed_z = utils.standard_normal_sample(self.trans_size)
 
     def _build_net(self, input_size):
         _, c, h, w = input_size
@@ -190,6 +189,7 @@ class ImplicitFlow(nn.Module):
 
     def forward(self, input, logpx=None, inverse=False, classify=False, restore=False):
         if inverse:
+            print(input.shape)
             assert torch.is_tensor(input)
             if input.shape[1:] == self.fixed_z.shape[1:]:
                 fixed_z = input
