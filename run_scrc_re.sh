ee0bd4768a9e7e616d61dd221f73ebe282be5b23
musikisomorphie <wujiqing9@gmail.com>
2021-08-20 16:08:39 +0200

    refactor the codes

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 deepspeed train_re.py --cuda  --data scrc --deepspeed_config config_re.json \
+CUDA_VISIBLE_DEVICES=4,5,6,7 --master_port 50001 deepspeed train_re.py --cuda  --data scrc --deepspeed_config config_re.json \
     --actnorm True --task hybrid --nworkers 2 \
     --nblocks 12-12-12 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_re_alpha/' --imagesize 256 \
