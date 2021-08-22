093fd1ec077b77ecdd3793c856e38e1e8dc70558
musikisomorphie <wujiqing9@gmail.com>
2021-08-22 15:31:09 +0200

    fix im issue

--- run_scrc_im.sh
+++ run_scrc_im.sh
@@ -1,8 +1,8 @@
 deepspeed --include=localhost:0,1,2,3 --master_port 50123 train_im.py --cuda  --data scrc --deepspeed_config config_im.json \
     --actnorm True --task hybrid --nworkers 2 \
-    --nblocks 8-8-8 --print-freq 120 --factor-out True --squeeze-first True \
+    --nblocks 12-12-12 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_im_alpha/' --imagesize 128 \
-    --dataroot /raid/jiqing/Data/SCRC/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
+    --dataroot /home/histopath/Data/SCRC_nuclei/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
     --env '201' --aug 'rr' --inp 'i' \
-    --idim '512' --act 'swish' --kernels '3-1-3' --vnorms '2222' --fc-end False --coeff 0.9 --n-exact-terms 10 \
+    --idim '512' --act 'swish' --kernels '3-1-3' --vnorms '2222' --fc-end False --fc False --coeff 0.9 --n-exact-terms 10 \
 
