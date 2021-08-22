b59a02fddcd6fd1fe2655e5a97d413776028ba33
musikisomorphie <wujiqing9@gmail.com>
2021-08-22 13:59:42 +0200

    add cell mask

--- run_scrc_re03.sh
+++ run_scrc_re03.sh
@@ -1,6 +1,6 @@
 deepspeed --include=localhost:0,1,2,3 --master_port 50123 train_re.py --cuda  --data scrc --deepspeed_config config_re.json \
     --actnorm True --task hybrid --nworkers 2 \
-    --nblocks 12-12-12 --print-freq 120 --factor-out True --squeeze-first True \
+    --nblocks 10-12-12 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_re_alpha_100/' --imagesize 256 \
     --dataroot /raid/jiqing/Data/SCRC/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
     --env '201' --aug 'rr' --inp 'i' \
