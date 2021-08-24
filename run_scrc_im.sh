2f7bd4630d209aedc5d216f866954bb64495010c
musikisomorphie <wujiqing9@gmail.com>
2021-08-24 14:13:19 +0200

    refactor the codes

--- run_scrc_im.sh
+++ run_scrc_im.sh
@@ -3,6 +3,6 @@ deepspeed --include=localhost:4,5,6,7 --master_port 54567 train_im.py --cuda  --
     --nblocks 8-8-8 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_im_alpha/' --imagesize 128 \
     --dataroot /raid/jiqing/Data/SCRC/ --batchsize 16 --val-batchsize 32 --nepochs 100 \
-    --env '201' --aug 'rr' --inp 'im' --scale-factor 2 --flow imflow \
+    --env '201' --aug 'rr' --inp 'im' --scale-factor 4 --flow imflow \
     --idim '512' --act 'swish' --kernels '3-1-3' --vnorms '2222' --fc-end False --fc False --coeff 0.9 --n-exact-terms 10 \
 
--- run_scrc_im_local.sh
+++ run_scrc_im_local.sh
@@ -3,6 +3,6 @@ deepspeed --include=localhost:0 --master_port 50123 train_im.py --cuda  --data s
     --nblocks 2-2-2 --print-freq 120 --factor-out True --squeeze-first True \
     --save 'experiments/scrc_im_alpha/' --imagesize 128 \
     --dataroot /home/histopath/Data/SCRC_nuclei/ --batchsize 2 --val-batchsize 4 --nepochs 100 \
-    --env '201' --aug 'rr' --inp 'im' --scale-factor 2 --flow imflow \
+    --env '201' --aug 'rr' --inp 'im' --scale-factor 4 --flow imflow \
     --idim '512' --act 'swish' --kernels '3-1-3' --vnorms '2222' --fc-end False --fc False --coeff 0.9 --n-exact-terms 10 \
 
