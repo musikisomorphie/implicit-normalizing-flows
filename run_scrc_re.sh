243924b162830a021dffa7621f30ba35df989d30
musikisomorphie <wujiqing9@gmail.com>
2021-08-30 13:39:36 +0200

    refactor the semisupervised

--- run_scrc_re.sh
+++ run_scrc_re.sh
@@ -4,4 +4,4 @@ deepspeed --include=localhost:0,1,2,3 --master_port 50123 train_re.py --cuda  --
     --flow reflow --classifier resnet --scale-factor 2 --env '201' --aug 'rr'  \
     --inp 'im' --oup 'cms' --couple-label False --imagesize 128 --batchsize 16 \
     --actnorm True --task hybrid --nworkers 2 --val-batchsize 32 --nepochs 100 \
-    --nblocks 12-12-12 --print-freq 120 --factor-out True --squeeze-first True 
+    --nblocks 12-12-12 --print-freq 120 --factor-out True --squeeze-first True
