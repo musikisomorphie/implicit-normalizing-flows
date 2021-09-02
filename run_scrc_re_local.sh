eeb36543414cc6b8c6cfe2a8113a81f817b190a2
musikisomorphie <wujiqing9@gmail.com>
2021-09-02 17:35:48 +0200

    minor change

--- run_scrc_re_local.sh
+++ run_scrc_re_local.sh
@@ -2,7 +2,6 @@ deepspeed --include=localhost:0 --master_port 50123 train_re.py --cuda  --data s
     --save 'experiments/scrc_resize64_circle/' \
     --dataroot /home/histopath/Data/SCRC_nuclei/ \
     --flow reflow --classifier resnet --scale-factor 2 --env '201' --aug 'r'  \
-    --inp 'im' --oup 'cms' --couple-label False --imagesize 64 --batchsize 8 \
+    --inp 'i' --oup 'cms' --couple-label False --imagesize 64 --batchsize 8 \
     --actnorm True --task hybrid --nworkers 2 --val-batchsize 8 --nepochs 100 \
     --nblocks 16-16-16 --print-freq 120 --factor-out True --squeeze-first True
-    
