52361080b13bc6bfffdbf8e1c4be04f91a6f5c04
musikisomorphie <wujiqing9@gmail.com>
2021-09-09 09:00:11 +0200

    integrate cell anno by adding them to tma image and use residual feature

--- run_scrc_re_all.sh
+++ run_scrc_re_all.sh
@@ -1,5 +1,5 @@
 deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port 50123 train_re.py --cuda  --data scrc --deepspeed_config config_re.json \
-    --save 'experiments/scrc_zero_pad_resize128_circle_all_add/' \
+    --save 'experiments/scrc_zero_pad_resize128_circle_all_add_res/' \
     --dataroot /raid/jiqing/Data/SCRC/ \
     --flow reflow --classifier resnet --scale-factor 2 --env '201' --aug 'rr'  \
     --inp 'im' --oup 'cms' --couple-label False --imagesize 128 --batchsize 16 \
