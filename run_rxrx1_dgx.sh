822dcd77735e906d743c3ea3592e4a21f2cd33f5
musikisomorphie <wujiqing9@gmail.com>
2021-09-21 15:12:05 +0200

    augment the training with reconstructed images

--- run_rxrx1_dgx.sh
+++ run_rxrx1_dgx.sh
@@ -2,6 +2,6 @@ deepspeed --include=localhost:0,1 --master_port 50123 train_re.py --cuda  --deep
     --dataset rxrx1 --dataroot /raid/jiqing/Data/ --save 'experiments/' \
     --flow reflow --classifier resnet --shuffle-factor 2 --scale-factor 1 \
     --env '201' --imagesize 128 --batchsize 64 \
-    --nepochs 100 --nblocks 8-8-8 --factor-out --actnorm \
+    --nepochs 100 --nblocks 4-4-4-4 --factor-out --actnorm \
     --task hybrid --nworkers 2 --eval-batchsize 64 --print-freq 120 --vis-freq 400
     
